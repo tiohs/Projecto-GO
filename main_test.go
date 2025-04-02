ecdc5ccd49f4a0e7095cea267880017667e7df85
Hamilton silva <hamilton.silva@tis.ao>
2025-04-02 19:06:22 +0100

    New conceitos

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 	total := Soma(1, 2)
-	if total != 4 {
+	if total != 3 {
 		t.Errorf("Resultado esperado da soma Soma(1, 2) = %d, deveria ser 3", total)
 	}
 }
